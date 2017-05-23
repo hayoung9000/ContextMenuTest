485985d7acb970ec5a9c46ffcecd9574274b26a8
hayoung9000 <wjsgkdud9000@naver.com>
2017-05-23 20:36:20 +0900

    make menu

--- app/src/main/java/com/example/lg/contextmenutest/MainActivity.java
+++ app/src/main/java/com/example/lg/contextmenutest/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.lg.contextmenutest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout linear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
