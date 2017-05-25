b8a221aa51c9610598c6cd2f0bbdd61d2ae0e925
hayoung9000 <wjsgkdud9000@naver.com>
2017-05-25 19:25:50 +0900

    eventhandling

--- app/src/main/java/com/example/lg/contextmenutest/MainActivity.java
+++ app/src/main/java/com/example/lg/contextmenutest/MainActivity.java
@@ -1,21 +1,25 @@
 package com.example.lg.contextmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout linear;
+    Button but2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button but1=(Button)findViewById(R.id.but1);
-        Button but2=(Button)findViewById(R.id.but2);
+        but2=(Button)findViewById(R.id.but2);
+        linear=(LinearLayout)findViewById(R.id.linear);
         registerForContextMenu(but1);
         registerForContextMenu(but2);
     }
@@ -29,8 +33,32 @@ public class MainActivity extends AppCompatActivity {
             //menu.setHeaderIcon(R.drawable.icon1);
             menuInflater.inflate(R.menu.menu1,menu);
         }
-        if(v.getId()==R.id.but2){
+        if(v.getId()==R.id.but2)
             menuInflater.inflate(R.menu.menu2,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        super.onContextItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.menu1_item_red:
+                linear.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.menu1_item_blue:
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.menu1_item_pink:
+                linear.setBackgroundColor(Color.rgb(239,157,154));
+                return true;
+            case R.id.menu2_item_rotation:
+                but2.setRotation(90);
+                return true;
+            case R.id.menu2_item_size:
+                but2.setScaleY(1.5f);
+                but2.setScaleX(1.5f);
+                return true;
         }
+
+        return false;
     }
 }
